d931e26611e2f6b70f037065a4430dfa21665f3e
KoalasMarch <sasuke_kub@hotmail.com>
2021-02-11 15:57:20 +0700

    create seed and validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,18 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :email, presence: true
+  validates :password, presence: true
+
+  def as_json
+    json = {}
+    json[:email] = self.email
+    json
+  end
+
+  def downcase_email
+    self.email = self.email.downcase
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..10).to_a.each do |i|
+  User.create(email: "user#{i}@mail.com", password: 123456789)
+end
